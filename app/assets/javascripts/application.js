1f9e2540dc60fbf11cb153ebe398b675122a3204
Jiongye Li <jiongye_li@hms.harvard.edu>
2014-01-12 22:52:10 -0500

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,40 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+$('#locale a').click(function(){
+  var locale = $(this).attr("href");
+  var pathname = location.pathname;
+  if(pathname == "/"){
+    location.href = locale;
+  }else if(pathname.match(/^\/(zh-TW|en)/) != null){
+    pathname = pathname.replace(/^\/(zh-TW|en)/, locale);
+    location.href = pathname + location.search;
+  }else{
+    pathname = locale + pathname;
+    location.href = pathname + location.search;
+  }
+
+  return false
+})
+
+$("#email").html("<a href='mailto:webmaster@tiananntemple.org'>webmaster@tiananntemple.org</a>")
+
+$('.image_url').click(function(){
+  var locale = $('#locale_value').data("value");
+  var url = $(this).attr("href");
+  location.href = "/" + locale + url;
+  return false;
+})
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tianann::Application.config.secret_token = 'ad0be60761aaea3991989cc004268348b5755a6c2036ffd6c0e53f7f4aa88c6730224286b58a592d31dbe1e2d5ebeb02bc902180fd578608b45337f2da450e86'
