f0900a614169e4227c3f56c48a3f159c4a3f3e19
Jiongye Li <jiongye_li@hms.harvard.edu>
2014-10-26 13:55:52 -0400

    fixed config error

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ end
 
 module Tianann
   class Application < Rails::Application
-    config.sass.preferred_syntax = :sass
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
